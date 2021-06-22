78bb7d625d02ba16de602428c5ea1606b85b3bf8
Allan Presotto <allanpresottoifpr@gmail.com>
2021-06-22 13:11:33 -0300

    feat(schema): validations

--- lib/wabanex/user.ex
+++ lib/wabanex/user.ex
@@ -15,5 +15,10 @@ defmodule Wabanex.User do
   def changeset(params) do
     %__MODULE__{} # __MODULE__ = Wabanex.User
     |> cast(params, @fields)
+    |> validate_required(@fields)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:name, min: 2)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
   end
 end
