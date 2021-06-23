17e8b134823bc473b94d5427b0985a69c25c97d6
Allan Presotto <allanpresottoifpr@gmail.com>
2021-06-22 21:49:39 -0300

    feat(resolver): user create

--- lib/wabanex_web/resolvers/user.ex
+++ lib/wabanex_web/resolvers/user.ex
@@ -1,3 +1,4 @@
 defmodule WabanexWeb.Resolvers.User do
+  def get(%{input: params}, _context), do: Wabanex.Users.Get.call(params)
   def get(%{id: user_id}, _context), do: Wabanex.Users.Get.call(user_id)
 end
